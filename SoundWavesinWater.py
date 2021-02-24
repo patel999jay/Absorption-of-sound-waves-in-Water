e461214df86340c27521761bab4d509956532a7c
Jay Patel <jy650701@dal.ca>
2021-02-24 18:07:52 -0400

    Update SoundWavesinWater.py

--- SoundWavesinWater.py
+++ SoundWavesinWater.py
@@ -6,17 +6,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-f= np.arange(1e-2,10e3) # in kHz
+f= np.arange(1e-2,10e3) # frequency range in kHz
 S= 35 # salinity
 T= 10 # temmperature in degree centi grad
 pH= 7.8  # Water pH level
 theta= 273+T   # Angle 
 D= 500 # Depth in meter
-c= 1412+3.21*T+1.19*S+0.0167*D  # Sound speed in Water  1500 m/s
+c= 1412+3.21*T+1.19*S+0.0167*D  # Sound speed in Water 1500 m/s
  
 # Boric Acid Contribution 
 A1= 8.86/c*10**(0.78*pH-5) # Absorption in Boric acid
-p1= 1  # pressur in Boric acid
+p1= 1  # Pressure in Boric acid
 f1= 0.78* np.sqrt(S/35)*10**(T/26) # frequency in Boric acid
 
 # MgSO_4 Contribution 
@@ -29,7 +29,7 @@ A3= 3.964*10**(-4)- 1.146* 10**(-5)*T +1.45*10**(-7)*T**2-6.5*10**(-10)*T**3 # A
 p3= 1-3.83*10**(-5) * D+4.9*10**(-10) *D**2  # Pressure in Pure water 
 
 Boric_acid= (A1*p1*f1*f)**2 /( f1**2+f**2)  # Formula for Boric acid  %%% Absorption of Boric acid
-Magnesium_sulphate= A2*p2*f2*(f**2)/ ((f2**2)+f**2) #Formula for Magnesium sulphate  %%%% Absorption of Megnesium sulphates
+Magnesium_sulphate= A2*p2*f2*(f**2)/ ((f2**2)+f**2) #Formula for Magnesium sulphate  Absorption of Magnesium sulphates
 Pure_water= A3*p3*f**2 # Formula for pure water   # Absorption of Water
 Total_absorption= Boric_acid + Magnesium_sulphate + Pure_water  #  Total Absorption
 
